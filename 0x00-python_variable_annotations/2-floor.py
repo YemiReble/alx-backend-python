f510b33db359d1a2cec93dd4d89fc4d477533eda
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-08-03 17:40:02 +0000

    Floor Updates

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,9 @@
-#!/usr/bin/python3
-"""Type annotation floor Function"""
+#!/usr/bin/env python3
+""" Type annotation floor Function """
 
 
 def floor(n: float) -> int:
-    """This function only returns a float"""
+    """This function only returns a float
+    n: The float parameter
+    """
     return int(n)
