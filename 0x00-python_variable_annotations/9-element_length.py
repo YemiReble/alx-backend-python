2b5f5636f03bd82c1a5b51fc296fc9a71341205b
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-08-04 14:49:00 +0000

    A function with time annotation that iterate through a list and return it length

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Type Annotation Function element_length """
+
+
+from typing import List, Sequence, Tuple, Iterable
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ This function is weird but all it does is to iterate through
+    any given lists """
+    return [(i, len(i)) for i in lst]
