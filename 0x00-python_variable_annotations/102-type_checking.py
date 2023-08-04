4317a255571aa71ab2bbd2d49defbe05bb96b6aa
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-08-04 16:16:43 +0000

    Type Checking - Zoom Array

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Type Annotation Function safely_get_value from a dict """
+
+
+from typing import Any, List, Tuple
+
+
+def zoom_array(lst: Tuple[int], factor: int = 2) -> List:
+    """ Iterate and zoom in through a list 2x """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
