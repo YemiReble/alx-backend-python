ebe89b331d97e4124ae82e439d0d51c527bf92b9
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-08-04 15:46:18 +0000

    Function that safely retrieve data from a dictionary

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-""" Type Annotation Function safely_get_value """
+""" Type Annotation Function safely_get_value from a dict """
 
 
-from typing import Mapping, Any, Union
+from typing import Mapping, Any, Union, TypeVar
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[~T, None]) -> Union[Any, ~T]:
+
+T = TypeVar('T')
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]) -> Union[Any, T]:
     """ This function safely retrieve a value from a dictionary """
     if key in dct:
         return dct[key]
