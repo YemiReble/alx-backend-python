434eb28137fd93124e11b105f7dcf4a0690b01e3
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-08-04 13:35:45 +0000

    Variable Annotation

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -2,13 +2,7 @@
 """ Type annotation variables Function """
 
 
-# Variable Annotations
-#a = int(1)
-#pi = float(2.14)
-#i_understand_annotations = bool(True)
-#school = str("Holberton")
-
-a = 1
-pi = 2.14
-i_understand_annotations = True
-school = "Holberton"
+a: int = 1
+pi: float = 2.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
