8c7a6111480b25e79b6656722b9e2894d1892e30
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-08-05 09:12:08 +0000

    Version Update

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -7,5 +7,5 @@ from typing import List, Sequence, Tuple, Iterable
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """ This function is weird but all it does is to iterate through
-    any given lists """
+    any given lists and return it len """
     return [(i, len(i)) for i in lst]
