33f80a17fb9b7c46e865a6e9bc37037fa6f53fe3
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-08-08 13:02:31 +0000

    Async and Wait [First Use Case]

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,11 +6,11 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int=10) -> float:
     """ This Function deplays using any given value of the
     default value of 10 """
     # if max_delay is None:
-    #   max_delay = 10
+    #    max_delay = 10
     rand = random.uniform(0, max_delay)
     delay = await asyncio.sleep(rand)
     return float(rand)
