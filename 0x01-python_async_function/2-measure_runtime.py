9860017558e4e6bc21ae90ddae213298f1e8a72c
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-08-08 16:18:33 +0000

    Function that measure Run time of other functions

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Measuring the runtime of the previous function """
+
+
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """ This fucntion calculate the time that the imported
+    function takes to run """
+    start_time = time.time()
+    waiting = await wait_n(n, max_delay)
+    end_time = time.time()
+    run_time = start_time - end_time
+    return float(run_time / n)
