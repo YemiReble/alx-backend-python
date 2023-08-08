73df72644616f9a30150269a34495781bdfc672d
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-08-08 17:31:20 +0000

    Task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Using a regular function in Async """
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay) -> asyncio.Task:
+    """ Function that returns the async class """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
