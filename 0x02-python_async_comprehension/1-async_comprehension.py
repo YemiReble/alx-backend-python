83c15625056c291b3b76f59cf37963481ee70983
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-08-11 16:16:25 +0000

    Version Update

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" This file imports Import 'async_generator' from the previous
+file to create a coroutine
+"""
+
+import asyncio
+from typing import List
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """ some """
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result
