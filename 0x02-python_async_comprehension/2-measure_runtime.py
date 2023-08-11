cee8e8d5d599d226e02db536ecceb34203db2623
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-08-11 17:40:15 +0000

    Measure Function Run Time

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" This file measures the runtime of an async function
+"""
+
+
+import asyncio
+import time
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ This functio execute and """
+    start_time = time.time()
+    # for i in range(4):
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    
+    end_time = time.time()
+    
+    run_time = end_time - start_time
+    return run_time
