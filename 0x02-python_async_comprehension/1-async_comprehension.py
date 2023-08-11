363ce1a4e0d44c30cca1d7ad15035877335ea314
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-08-11 16:21:32 +0000

    Async Comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,7 +9,7 @@ async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """ some """
+    """ This funtion generates a list of 10 """
     result = []
     async for i in async_generator():
         result.append(i)
